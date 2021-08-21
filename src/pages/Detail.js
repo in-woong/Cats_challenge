58e0206fb21feb42963812f6547f6c2ef57fbceb
in-woong <78221769+in-woong@users.noreply.github.com>
2021-08-21 12:41:30 +0900

    Detail : async-wait

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,11 +1,11 @@
 import {useParams} from 'react-router-dom';
 import { CatApi } from '../utils/api';
 
-const Detail = () => {
+const Detail = async () => {
   const {id} = useParams();
   const myStorage = window.localStorage;
   const breed = localStorage.getItem('breeds');
-  const {data:{0:{breeds:{0: cat}}}} = CatApi.catDetail(`${id}`)
+  const {data:{0:{breeds:{0: cat}}}} = await CatApi.catDetail("beng")
   console.log(cat)
   return <h1>Detail</h1>;
 };
